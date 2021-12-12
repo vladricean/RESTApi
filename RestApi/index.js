c984f0f6271297a7f7d5b57de82a5555cf669ee3
Vlad <vlad.ricean@brivo.com>
2021-12-12 18:37:01 +0200

    -get/add dress by specified ID

--- RestApi/index.js
+++ RestApi/index.js
@@ -4,19 +4,13 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json())
 
-app.get('/', (req,res) => {
-    res.send('Hello there!!!');
-});
+
 
 app.get('/dress', (req, res) => {
     res.status(200).send({
         dress: '👗',
         size: 'large'
     })
-});
-
-app.get('/api/dresses', (req, res) => {
-    res.send([1, 2, 3, 4]);
 });
 
 app.post('/dress/:id', (req, res) => {
@@ -31,6 +25,37 @@ app.post('/dress/:id', (req, res) => {
         dress: `👗 with your ${logo} and ID of ${id}`,
     });
 
+});
+
+// NEW API
+
+const dresses = [
+    { id: 1, name: 'dress1'},
+    { id: 2, name: 'dress2'},
+    { id: 3, name: 'dress3'},
+];
+
+app.get('/', (req,res) => {
+    res.send('Hello there!!!');
+});
+
+app.get('/api/dresses', (req, res) => {
+    res.send(dresses);
+});
+
+app.get('/api/dresses/:id', (req, res) => {
+    const dress = dresses.find(c => c.id === parseInt(req.params.id));
+    if(!dress) res.status(404).send('The course with the given ID was not found.');
+    res.send(dress);
+});
+
+app.post('/api/dresses', (req, res) => {
+    const dress = {
+        id: dresses.length + 1,
+        name: req.body.name
+    };
+    dresses.push(dress);
+    res.send(dress);
 });
 
 app.listen(
